be63c228a82cb945c0d1cf5fbdb7c06dcb8d178f
kevin <Koss112283@gmail.com>
2020-12-31 05:04:50 -0500

    allow app to start when the computer powers up by adding shortcut to startup folder

--- TextSpeech/Program.cs
+++ TextSpeech/Program.cs
@@ -5,6 +5,9 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.IO;
+using IWshRuntimeLibrary;
 
 namespace TextSpeech
 {
@@ -15,7 +18,12 @@ namespace TextSpeech
         private static LowLevelKeyboardProc _proc = HookCallback;
         private static IntPtr _hookID = IntPtr.Zero;
         public static TextSpeaker speaker;
-        
+        const int CSIDL_COMMON_STARTUP = 0x0018;
+        const int CSIDL_STARTUP = 0x0018;
+
+        [DllImport("shell32.dll")]
+        static extern bool SHGetSpecialFolderPath(IntPtr hwndOwner, [Out] StringBuilder lpszPath, int nFolder, bool fCreate);
+
         [STAThread]
         static void Main()
         {
@@ -24,9 +32,91 @@ namespace TextSpeech
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             speaker = new TextSpeaker();
+            //check to see if the app.exe exist in the startup folder
+            CreateStartupShortcut();
             Application.Run(speaker);
             UnhookWindowsHookEx(_hookID);
+            
           
+        }
+        
+        public static string FindStartupFolder() 
+        {
+            String path;
+            //SHGetSpecialFolderPath(IntPtr.Zero, path, CSIDL_COMMON_STARTUP, false);
+            path = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            return path.ToString();
+        }
+        private static string getAppShortcutFileName() 
+        {
+            return Path.Combine(FindStartupFolder(), Application.ProductName) + ".lnk";
+        }
+        public static bool CheckStartupFileExist() 
+        {
+            return System.IO.File.Exists(getAppShortcutFileName());    
+        }
+        public static string Createlocalshortcut() 
+        {
+            //TODO: this method needs to be fixed
+            string shortcutPath = Application.StartupPath;
+            string shortcutLocation = System.IO.Path.Combine(shortcutPath, @"textSpeech" + ".lnk");
+
+            //Create all users desktop shortcut for application settings
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+            shortcut.TargetPath = Application.ExecutablePath;
+            shortcut.IconLocation = @"icon1";
+            shortcut.Description = "Description";
+            shortcut.Save();
+
+            return shortcutLocation;
+            
+        }
+        public static bool CreateStartupShortcut() 
+        {
+            //Createlocalshortcut();
+            bool retval = false;
+            string shortcutName = Createlocalshortcut();
+            FileInfo shortcutFile = new FileInfo(shortcutName);
+            FileInfo destination = new FileInfo(getAppShortcutFileName());
+
+            if (destination.Exists)
+            {
+                //run overwrite code
+                retval = CopyShortcut(shortcutFile,destination);
+            }
+            else if (!shortcutFile.Exists)
+            { 
+                MessageBox.Show("Unable to RunOnStartup becasue the local shortcut cant be found.");
+            }
+            else
+            {
+                retval = CopyShortcut(shortcutFile,destination);
+            }
+                
+
+            return retval; 
+        }
+        public static bool CopyShortcut(FileInfo src, FileInfo dest) 
+        {
+            bool retVal = false;
+            try
+            {
+                //System.IO.File.Copy(src.FullName, dest.FullName, true); you need to run this to overwrite the file
+                System.IO.File.Copy(src.FullName,dest.FullName);
+                retVal = true;
+            }
+            catch (UnauthorizedAccessException authEX) 
+            {
+                MessageBox.Show(string.Format("UnauthorizedAccessException: {0}",authEX.Message), "Copy Shortcut", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Copy Shortcut", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            return retVal;
         }
        /// <summary>
        /// This method sets up the windows hook for the keyboard proc
@@ -73,6 +163,7 @@ namespace TextSpeech
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
+
 
     }
     #endregion
--- TextSpeech/Properties/Resources.Designer.cs
+++ TextSpeech/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace TextSpeech.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap totallyradshowbg {
-            get {
-                object obj = ResourceManager.GetObject("totallyradshowbg", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
